db5865557afb84a87ffc622852e923a71a8a9250
LayxProud <49621292+LayxProud@users.noreply.github.com>
2022-04-09 16:21:50 +0300

    Add files via upload

--- Try-Throw-Parser/MYparser.cpp
+++ Try-Throw-Parser/MYparser.cpp
@@ -1,34 +1,37 @@
 #include <stdio.h>
+#include <vector>
+#include <algorithm>
 #include "MYlexer.h"
 
 extern int lookahead;
 extern std::string text;
+std::vector <std::string> throwExecptions;
 std::string nameOfException;
+std::vector <std::string> catchExceptions;
 std::string catchedException;
 bool tryTrue = true;
+bool catchedSomething = false;
+
+using namespace std;
 
 /*
 *  грамматика
 *
 * stmt ::= try { a' } cc
 * a' ::= a b
-* a ::= throw expr; | stmt
+* a ::= print expt; | throw expr; | stmt
 * b ::= a' | Epsilon
-* cc ::= catch ( expr ) { d' } cc'
+* cc ::= catch ( expr ) { a' } cc'
 * cc' ::= cc | Epsilon
-* d' ::= print expr; d
-* d ::= d' | Epsilon
 *
 */
 
 void stmt(bool firstTry);
-bool aI(bool flag);
+bool aI(bool flag, bool catchedSomething);
 void b(bool flag);
-void a(bool flag);
+void a(bool flag, bool catchedSomething);
 void cc(bool flag);
 void ccI();
-bool dI(bool flag);
-void d();
 
 // блок try
 void stmt(bool firstTry) {
@@ -38,34 +41,61 @@ void stmt(bool firstTry) {
 	}
 	scan();
 	if (lookahead != LPAREN) error("Expected {");
-	aI(true);
+	aI(true, catchedSomething);
 	if (lookahead != RPAREN) error("Expected }");
 	cc(true);
 }
 
 // а штрих
-bool aI(bool flag) {
-	a(flag);
+bool aI(bool flag, bool catchedSomething) {
+	a(flag, catchedSomething);
 	b(true);
 	return flag;
 }
 
 // а
-void a(bool flag) {
+void a(bool flag, bool catchedSomething) {
 	if (flag) {
 		scan();
 	}
+
+	string printText;
 	switch (lookahead){
 		case (THROW): 
 			scan();
-			nameOfException = text;	
+			nameOfException = text;
+			throwExecptions.push_back(nameOfException);
 			scan();
 			if (lookahead != TCHKZPT) error("Expected ;");
 		break;
 		case (TRY): 
 			stmt(false);
 		break;
-		default: error("Expected try or throw");
+		case (PRINT):
+			if (catchedSomething == false) {
+				scan();
+				printText = text;
+				cout << "Simple print: " << printText << endl;
+				scan();
+				if (lookahead != TCHKZPT) error("Expected ;");
+			}
+			else {
+				catchedSomething = false;
+				scan();
+				printText = text;
+				/*if (strcmp(nameOfException.c_str(), catchedException.c_str()) == 0)
+					cout << "Your output: " << printText << endl;*/
+				if (std::find(throwExecptions.begin(), throwExecptions.end(), catchedException) != throwExecptions.end()) {
+					/* v contains x */
+					cout << "Your output: " << printText << endl;
+				}
+				else
+					cout << "No such exception: " << catchedException << endl;
+				scan();
+				if (lookahead != TCHKZPT) error("Expected ;");
+			}
+		break;
+		default: error("Expected try or throw or print");
 	}
 }
 
@@ -74,8 +104,8 @@ void b(bool flag) {
 	if (flag && tryTrue) scan();
 	else if (!tryTrue)
 		tryTrue = !tryTrue;
-	if (lookahead == TRY || lookahead == THROW)
-		aI(false);
+	if (lookahead == TRY || lookahead == THROW || lookahead == PRINT)
+		aI(false, catchedSomething);
 }
 
 // блок catch
@@ -88,11 +118,12 @@ void cc(bool flag) {
 	if (lookahead != OPEN) error("Expected (");
 	scan();
 	catchedException = text;
+	catchedSomething = true;
 	scan();
 	if (lookahead != CLOSE) error("Expected )");
 	scan();
 	if (lookahead != LPAREN) error("Expected {");
-	dI(true);
+	aI(true, catchedSomething);
 	//if (flag) {
 		//scan();
 	//}
@@ -100,34 +131,13 @@ void cc(bool flag) {
 	ccI();
 }
 
-// d штрих
-bool dI(bool flag){
-	if (flag){
-		scan();
-		if (lookahead != PRINT) error("Expected print");
-	}
-	scan();
-	if (strcmp(nameOfException.c_str(), catchedException.c_str()) == 0)
-		std::cout << "Your output: " << text << std::endl;
-	scan();
-	if (lookahead != TCHKZPT) error("Expected ;");
-	d();
-	return flag;
-}
-
-// d
-void d(){
-	scan();
-	if (lookahead == PRINT)
-		dI(false);
-}
-
 // кетч штрих
 void ccI() {
 	scan();
 	if (lookahead == CATCH)
 		cc(false);
-	else if (lookahead == THROW) 
+	//else if (lookahead == THROW) 
+	else
 		tryTrue = false;
 }
 
